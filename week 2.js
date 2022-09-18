4d8f54f1d7fdb838a864572641734cf561c39eed
Jerblake-debarros <113395881+Jerblake-debarros@users.noreply.github.com>
2022-09-17 21:03:50 -0700

    Update week 2.js

--- week 2.js
+++ week 2.js
@@ -1,6 +1,6 @@
 var showYear;//sets var showYear for week2 program to use
-setInterval (//set interval is a loop that repeats every minute thanks to https://www.w3schools.com/jsref/met_win_setinterval.asp
+setInterval (//set interval is a loop that repeats every  half minute thanks to https://www.w3schools.com/jsref/met_win_setinterval.asp
     function(){//this funtion shows date and time as alert
     var today = new Date();
-    alert ("The date and time today is "+ today);
-              }, 60000);
+    alert ("The date and time today is "+ today); 
+              }, 30000);//It does as well take 30 seconds to start but I wanted to prove that it is looping
